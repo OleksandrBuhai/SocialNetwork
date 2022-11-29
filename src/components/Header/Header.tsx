14c9c34ba73e940fe51cc91038b9f4cfb31dae9f
Oleksandr Buhai <oleksandr.buugay@gmail.com>
2022-11-29 21:10:59 +0100

    html(grid construction) + components

---
+++ src/components/Header/Header.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Header = () => {
+    return (
+        <header className={"header"} >
+            <img src={"https://upload.wikimedia.org/wikipedia/commons/3/3c/IMG_logo_%282017%29.svg"}/>
+        </header>
+    );
+};
+
+export default Header;
---
+++ src/components/Profile/Profile.tsx
@@ -0,0 +1,28 @@
+import React from 'react';
+
+const Profile = () => {
+    return (
+        <div className={'content'}>
+            <img src={'https://victoria.mediaplanet.com/app/uploads/sites/102/2019/07/mainimage-26-888x500.jpg'}/>
+            <div>
+                ava+discriptions
+            </div>
+            <div>
+                My Post
+                <div>
+                    My Post1
+                </div>
+                <div>
+                    My post2
+                </div>
+                <div>
+                    My post3
+                </div>
+            </div>
+
+        </div>
+
+    );
+};
+
+export default Profile;
