9507b08f34e0e6c638b0790a3de291710f11cf08
Oleksandr Buhai <oleksandr.buugay@gmail.com>
2022-12-05 18:27:48 +0100

    Dialogs + Dialogs.module.css

--- src/components/Dialogs/Dialogs.tsx
+++ src/components/Dialogs/Dialogs.tsx
@@ -1,9 +1,73 @@
 import React from 'react'
+import c from "./Dialogs.module.css"
+import {NavLink} from "react-router-dom";
 
-export const Dialogs = () => {
+type DialogItemPropsType = {
+    name:string
+    id: number
+}
+
+export const DialogItem = (props: DialogItemPropsType) => {
+
+    let path = "/dialogs" + props.id
     return (
-        <div>
-            hello
+        <div className={c.dialog + ' ' + c.active}>
+            <NavLink to={path}>{props.name}</NavLink>
         </div>
     )
 }
+
+type MessagePropsType = {
+    message: string
+}
+
+export const Message = (props: MessagePropsType) => {
+    return (
+        <div className={c.dialog}>{props.message}</div>
+    )
+}
+type DialogsPropsType = {
+
+}
+
+export const Dialogs = (props:DialogsPropsType) => {
+
+    let dialogsData = [
+        {id: 1, name: 'Dimych'},
+        {id: 2, name: 'Andrew'},
+        {id: 3, name: 'Sveta'},
+        {id: 4, name: 'Sasha'},
+        {id: 5, name: 'Viktor'},
+        {id: 6, name: 'Valera'}
+    ]
+
+    let messagesData = [
+        {id: 1, message: 'Hi'},
+        {id: 2, message: 'How is your it-kamasutra?'},
+        {id: 3, message: 'Yo'},
+        {id: 4, message: 'Yo'},
+        {id: 5, message: 'Yo'}
+    ]
+
+    return (
+        <div className={c.dialogs}>
+            <div className={c.dialogsItems}>
+                <DialogItem name={dialogsData[0].name} id={dialogsData[0].id} />
+                <DialogItem name={dialogsData[1].name} id={dialogsData[1].id}/>
+            </div>
+            <div className={c.messages}>
+                <Message message={messagesData[0].message}/>
+                <Message message={messagesData[1].message}/>
+            </div>
+        </div>
+    )
+
+}
+
+
+
+
+
+
+
+
