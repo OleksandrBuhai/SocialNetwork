62a3d1d226c5bfc02b9d773bdcf19e60e98fc82b
Oleksandr Buhai <oleksandr.buugay@gmail.com>
2023-03-14 20:47:58 +0100

    huy

--- src/StoreContext.ts
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import store, {storeType} from "./redux/redux-state";
-
-
-const StoreContext = React.createContext(store as storeType)
-
-
-
-export default StoreContext
